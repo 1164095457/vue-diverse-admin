021705e7b8015483c9f98fc80ed350c084366c2c
lin <1164095457@qq.com>
2023-01-06 18:30:10 +0800

    修改首页内容

--- src/views/home/components/HomeTable.vue
+++ src/views/home/components/HomeTable.vue
@@ -1,59 +1,35 @@
 <template>
     <div class="card" style="height:400px;">
-        <el-table :data="tableData" border style="width: 100%;height:100%">
-    <el-table-column prop="date" label="日期" width="120" />
-    <el-table-column prop="name" label="姓名" width="120" />
-    <el-table-column prop="address" label="地址" />
-  </el-table>
+        <el-tag type="success">这周的工作计划</el-tag>
+        <el-divider />
+    <el-table :data="tableData" border style="width: 100%;">
+        <el-table-column prop="date" label="日期" width="120" />
+        <el-table-column prop="name" label="计划" />
+    </el-table>
     </div>
 </template>
 
 <script setup>
 const tableData = [
   {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: '广东省',
+    date: '星期一',
+    name: '星期一计划'
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: '广东省',
+    date: '星期二',
+    name: '星期二计划'
   },
   {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: '广东省',
+    date: '星期三',
+    name: '星期三计划'
   },
   {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: '广东省',
+    date: '星期四',
+    name: '星期四计划'
   },
   {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: '广东省',
+    date: '星期五',
+    name: '星期五计划'
   },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: '广东省',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: '广东省',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: '广东省',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: '广东省',
-  }
 ]
 </script>
