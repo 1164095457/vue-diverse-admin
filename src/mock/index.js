54025451f2b4a3cd279b6bf8703a2e14b583ffe2
lin <1164095457@qq.com>
2023-01-11 14:48:51 +0800

    mock数据

--- src/mock/index.js
+++ src/mock/index.js
@@ -6,17 +6,17 @@ import DynamicRouter from "@/assets/json/routerList.json";
 Mock.mock("/api/router/list", () => {
   {
     return Mock.mock({
-      code:200,
-      list:DynamicRouter.data
+      code: 200,
+      list: DynamicRouter.data
     });
   }
 });
 
 Mock.mock("/api/login", () => {
-    {
-      return Mock.mock({
-        name: "admin",
-        token:'tokentest'
-      });
-    }
-  });
+  {
+    return Mock.mock({
+      name: "admin",
+      token: 'tokentest'
+    });
+  }
+});
