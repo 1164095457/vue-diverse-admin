80c63623aefcd6c27766ac7d812c9af30c9f277a
lin <1164095457@qq.com>
2023-01-11 14:50:26 +0800

    项目配置

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,3 @@
-// ? 全局不动配置项 只做导出不做修改
-
 // * 首页地址（默认）
 export const HOME_URL = "/home/index";
 
@@ -7,10 +5,7 @@ export const HOME_URL = "/home/index";
 export const LOGIN_URL = "/login";
 
 // * 默认主题颜色
-export const DEFAULT_PRIMARY = "#009688";
+export const DEFAULT_PRIMARY = "#409EFF";
 
 // * Tabs（白名单地址，不需要添加到 tabs 的路由地址）
 export const TABS_WHITE_LIST = ["/403", "/404", "/500", LOGIN_URL];
-
-// * 高德地图key
-export const MAP_KEY = "";
