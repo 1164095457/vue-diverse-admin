7cad99cf77376ea04eae921ad32f955d836ce5dd
lin <1164095457@qq.com>
2023-02-04 15:25:24 +0800

    读取文件

--- src/utils/util.js
+++ src/utils/util.js
@@ -151,3 +151,18 @@ export const getAllBreadcrumbList = (menuList, result = {key:''}, path = []) => 
 	}
 	return result;
 };
+
+
+/**
+ * @description 递把文件按照二进制方式读取
+ * @param {File} file 文件源
+ */
+export const readFile = file =>{
+	return new Promise(resolve => {
+		let reader = new FileReader();
+		reader.readAsBinaryString(file);
+		reader.onload = ev =>{
+			resolve(ev.target.result)
+		}
+	})
+}
