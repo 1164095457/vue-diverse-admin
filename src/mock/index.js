4713b9b1dbaed6acead7a39a07e2a5e8471fb00c
林泽权 <1164095457@qq.com>
2023-10-08 11:26:10 +0800

    style：format

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,31 +1,30 @@
-import Mock from 'mockjs'
+import Mock from "mockjs";
 import DynamicRouter from "@/assets/json/routerList.json";
 import authButton from "@/assets/json/authButton.json";
 
-
 Mock.mock("/api/router/list", () => {
-  {
-    return Mock.mock({
-      code: 200,
-      list: DynamicRouter.data
-    });
-  }
+	{
+		return Mock.mock({
+			code: 200,
+			list: DynamicRouter.data
+		});
+	}
 });
 
 Mock.mock("/api/login", () => {
-  {
-    return Mock.mock({
-      name: "admin",
-      token: 'tokentest'
-    });
-  }
+	{
+		return Mock.mock({
+			name: "admin",
+			token: "tokentest"
+		});
+	}
 });
 
 Mock.mock("/api/auth/button", () => {
-  {
-    return Mock.mock({
-      code: 200,
-      list: authButton.data
-    });
-  }
+	{
+		return Mock.mock({
+			code: 200,
+			list: authButton.data
+		});
+	}
 });
